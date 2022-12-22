ae1dd027bdd03ee333cc52d752d97efb173bfd41
Irfan Yanbukhtin <irfanfy@code.edu.az>
2022-12-22 19:27:57 +0400

    Add project files.

---
+++ Project.3.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project._3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Change");
+        }
+    }
+}
